4bf4914c999eb78d72ad7eaa4c5ff6b521cc7162
sinheungheung <s2120@e-mirim.hs.kr>
2022-04-01 10:08:22 +0900

    매크로함수완성

--- C++_201/02-01매크로함수.cpp
+++ C++_201/02-01매크로함수.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-#define jegob(A) A*A
+#define jegob(A) ((A)*(A))
 
 int main(void)
 {
-	cout << jegob(3) << endl;
-	cout << 3 * 3 << endl; //위와 같음
+	cout << jegob(3+1) << endl; //4*4와 같음
+	cout << ((3+1) * (3+1)) << endl; //위와 같음
 
 }
