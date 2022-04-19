bf40bd53bba14bfab5fba7a7e8d6a015bfc18af2
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-04-19 11:56:43 +0900

    파일이름 바꾸기

---
+++ 03-01 class 생성자.cpp
@@ -0,0 +1,28 @@
+#include<iostream> //cout,endl 사용
+#include<string>
+
+using namespace std;
+
+class Student
+{
+private:
+	int nHakbun;
+	string sName;
+
+	// 생성자: 객체가 생성되면 자동으로 호출되는 함수
+	// 반환형을 갖고있지 않는다
+
+public:
+	Student()
+	{
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
+};
+int main(void)
+{
+	
+	Student stu;
+	
+}
