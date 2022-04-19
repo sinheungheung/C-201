3a1668292ec6de8781a9829cbb7c7a8f71ea66e1
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-04-19 12:03:54 +0900

    멤버함수를 class 바깥으로

--- 03-01 class 생성자.cpp
+++ 03-01 class 생성자.cpp
@@ -9,27 +9,36 @@ private:
 	int nHakbun;
 	string sName;
 
-	// 생성자: 객체가 생성되면 자동으로 호출되는 함수
-	// 반환형을 갖고있지 않는다
+	
 
 public:
-	Student()
-	{
+	// 생성자: 객체가 생성되면 자동으로 호출되는 함수
+	// 반환형을 갖고있지 않는다
+	Student();
+
+	void show();
+	
+};
+
+
+Student::Student()
+{
+	
 		nHakbun = 1234;
 		sName = "이사랑";
 		cout << "학번이 등록되었습니다." << endl;
-	}
-	void Show()
-	{
-		// 멤버함수
-		cout << "학번은 " << nHakbun << "입니다" << endl;
-		cout << "학번은 " << sName << "입니다" << endl;
-	}
-};
+	
+}
+void Student::show()
+{
+	// 멤버함수
+	cout << "학번은 " << nHakbun << "입니다" << endl;
+	cout << "학번은 " << sName << "입니다" << endl;
+}
 int main(void)
 {
 	
 	Student stu;
-	stu.Show();
+	stu.show();
 	
 }
