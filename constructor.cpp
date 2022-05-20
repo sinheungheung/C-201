4e7b0cabc9eb055821b016a2bedf117becc18c1e
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-05-20 10:20:04 +0900

    Person 클래스를 상속받는 Student클래스

--- constructor.cpp
+++ constructor.cpp
@@ -6,25 +6,46 @@ class Person {
 private:
 	string sName;
 	int nAge;
+protected:
+	int nAge;
 public:
 	Person();
 	Person(string name, int age);
 	void hi();
 };
+class Student :public Person {
+private:
+	int nHakbun;
+
+public:
+	Student(); // 생성자
+	Student(string name, int age, int nHakbun); // 매개변수 넣은 생성자
+	void study();
+};
+
+Student::Student() : Person() {
+	nHakbun = 0;
+}
+Student::Student(string name, int age, int hakbun)
+	:Person(name,age),nHakbun(hakbun){}
 
 Person::Person() {
 	sName = "익명";
 	nAge = 0;
 }
-
-
 Person::Person(string name, int age) 
 	:sName(name), nAge(age)	{
 }
 
 void Person::hi() {
-	cout << "이름: " << sName
-		 << "나이: " << nAge << endl;
+	cout << "이름: " << sName;
+	cout << "나이: " << nAge << endl;
+}
+
+void Student::study() {
+	hi(); // hi함수 호출
+	cout << "학번: " << nHakbun;
+	nAge = 100;
 }
 
 int main() {
@@ -37,7 +58,25 @@ int main() {
 	// 객체 배열
 	Person* people = new Person[5];
 	people[0].hi();
-
+	Student stu;
+	// stu.nAge = 100;
 	delete person;
 	delete[] people;
+
+	// 접근제어자
+	class CBox {
+	private:
+		double width;
+	};
+	// get,set
+	class ChilBox :CBox {
+	public: 
+		void setWidth(double w) {
+		width = w;
+		}
+	};
 }
+// 163pg
+// private: 다
+// protected: 나
+// public: 가
