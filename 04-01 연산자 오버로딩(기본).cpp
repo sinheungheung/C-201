3525d03954cd06c827363271347aa9a1faacb1a4
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-06-03 10:24:49 +0900

    헤더 파일 분활

--- 04-01 연산자 오버로딩(기본).cpp
+++ 04-01 연산자 오버로딩(기본).cpp
@@ -1,42 +1,7 @@
 #include<iostream> // cout,endl 사용
+#include "04-01.h"
 using namespace std;
 
-class Point
-{
-private:
-	int mX;
-	int mY;
-public:
-	// 생성자
-	Point(int _x, int _y);
-	// 매개변수가 없는 생성자
-	Point(void);
-
-	// 연산자 오버로딩
-	Point operator+(const Point& rhs);//right hand side: 오른쪽 방면
-
-	void print(void);
-};
-// 생성자 정의:초기화 
-Point::Point(int _x, int _y) :mX(_x), mY(_y) {}
-// 매개변수 없는 생성자 정의
-Point::Point(){}
-
-void Point::print(void)
-{
-	cout <<"("<< mX << ", "<<mY <<")"<<endl;
-}
-//{
-//	mX = _x;
-//	mY = _y;
-//};
-Point Point::operator+(const Point& rhs)
-{
-	Point p; // 매개변수 없는 생성자 필요
-	p.mX = mX + rhs.mX;
-	p.mY = mY + rhs.mY;
-	return p;
-}
 int main(void)
 {
 	Point p1(1, 2);
