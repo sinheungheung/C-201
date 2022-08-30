6ca2b1e580a549f224bfb5e68f3cf54aad7929e3
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-08-30 12:11:18 +0900

    순수가상함수(추상화)

---
+++ 06-03 순수가상함수.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat(void) = 0;
+	virtual void roar(void) = 0;
+	virtual void walk(void) = 0;
+	// ÀÚ¹ÙÀÇ Ãß»ó¸Þ¼Òµå¿Í ¶È°°´Ù 
+
+private:
+	string name;
+	int sex;
+	int age;
+};
+
+class Tiger : public Animal {
+public:
+	void eat(void) override { cout << "È£¶ûÀÌ ³È³È" << endl; }
+	void roar(void) override { cout << "¾îÈï" << endl; }
+	void walk(void) override { cout << "È£¶ûÀÌ ´ç´ç" << endl; }
+	};
+
+class Dog : public Animal {
+public:
+	void eat(void) override { cout << "°³ ÂÁÂÁ" << endl; }
+	void roar(void) override { cout << "¸Û¸Û" << endl; }
+	void walk(void) override { cout << "°³ »ç»Ó" << endl; }
+};
+
+void main(void)
+{
+	// Animal* animal = new Animal(); // ½ÇÃ¼°¡ ¾ø´Â °´Ã¼´Â »ý¼ººÒ°¡ 
+	Animal* animal = new Tiger();
+	delete animal;
+}
