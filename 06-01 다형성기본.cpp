ad151c14ff3e33137b97fbb294e73bdb2cc613e3
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-08-30 10:53:10 +0900

    다형성 기본

---
+++ 06-01 다형성기본.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	void eat(void) { cout << "µ¿¹°¸Ô¾î" << endl; }
+	void roar(void) { cout << "µ¿¹°Â¢¾î" << endl; }
+	void walk(void) { cout << "µ¿¹°°É¾î" << endl; }
+
+private:
+	string name;
+	int sex;
+	int age;
+};
+
+class Tiger : public Animal {
+public:
+	void roar(void) { cout << "¾îÈï" << endl; }
+};
+class Dog : public Animal {
+public:
+	void roar(void) { cout << "¸Û¸Û" << endl; }
+};
+void main(void)
+{
+	Animal animal;
+	animal.roar(); // "µ¿¹°Â¢¾î"
+	
+	Dog dog;
+	dog.roar(); // "¸Û¸Û"
+}
