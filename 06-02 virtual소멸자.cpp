249d150851495a9f6b6c885629ac1471b37a0b2a
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-08-30 11:52:18 +0900

    소멸자 앞에는 무조건 virtual 키워드 넣기

--- 06-02 virtual소멸자.cpp
+++ 06-02 virtual소멸자.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Animal {
 public:
 	Animal() { cout << "Animal 积己" << endl; }
-	~Animal() { cout << "Animal 家戈" << endl; }
+	virtual ~Animal() { cout << "Animal 家戈" << endl; }
 	void eat(void) { cout << "悼拱冈绢" << endl; }
 };
 
 class Tiger : public Animal {
 public:
 	Tiger() { cout << "Tiger 积己" << endl; }
-	~Tiger() { cout << "Tiger 家戈" << endl; }
+	virtual ~Tiger() { cout << "Tiger 家戈" << endl; }
 	};
 
 void main(void)
