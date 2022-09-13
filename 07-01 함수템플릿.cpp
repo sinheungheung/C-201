9734ac9b56b20e619b37a63a2089af7772998319
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-09-13 10:55:26 +0900

    템플릿 하기 전

---
+++ 07-01 함수템플릿.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int sum(int a, int b);
+float sum(float a, float b);
+string sum(string a, string b);
+
+int main(void)
+{
+	cout << sum(1, 2) << endl;
+	cout << sum(1.f, 2.f) << endl;
+	cout << sum("1", "2") << endl;
+}
+float sum(float a, float b)
+{
+	return a + b;
+}
+
+string sum(string a, string b)
+{
+	return a + b;
+}
