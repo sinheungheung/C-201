7eee62a29f6d155e8b4dcb4d66bce5f0f99b4f9d
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-09-23 08:57:25 +0900

    클래스 템플릿

--- 07-02 클래스 템플릿.cpp
+++ 07-02 클래스 템플릿.cpp
@@ -3,32 +3,30 @@
 
 using namespace std;
 
-template <typename T>
-class MyData {
-	
+template <typename S, typename T>
+class Character {
 public:
-	void setData(T pdata)
+	Character(S _name, T _hp, T _offense, T _defense) 
+		: name(_name), hp(_hp), offense(_offense), defense(_defense) {}
+	void print(void)
 	{
-		mydata = pdata;
-	}
-	T getData()
-	{
-		return mydata;
+		cout << "이름 " << name << endl;
+		cout << "체력 " << hp << endl;
+		cout << "공격 " << offense << endl;
+		cout << "방어 " << defense << endl;
+
 	}
 private:
-	T mydata;
+	S name;
+	T hp;
+	T offense;
+	T defense;
 };
 
 int main(void)
-{
-	MyData<int> mydata1;
-	MyData<double> mydata2;
-
-	mydata1.setData(5);
-	cout << mydata1.getData() << endl;
-
-	mydata2.setData(5.5);
-	cout << mydata2.getData() << endl;
+{	
+	Character<string, int> juin("주인공", 1, 10000, 999999);
+	juin.print();
 
 	return 0;
 }
