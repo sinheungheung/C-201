bb85aeef059ecefcf7923dd740f965b84e57acd1
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-10-25 10:49:36 +0900

    전통적인 반복문

---
+++ 09-02 범위기반 for.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void main(void)
+{
+	int arr[4] = { 10, 20, 30, 40 };
+	for (int i = 0; i < 4; i++) {
+		cout << arr[i] << " ";
+	}
+}
